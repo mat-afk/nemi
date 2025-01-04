6df75ac3dcd02171458c96e20ca2db2d104edb8a
Mateus <mateusc.ramos@outlook.com>
2025-01-03 23:37:18 -0300

    refactor: add .env to path

--- api/src/main/java/br/com/nemi/NemiApplication.java
+++ api/src/main/java/br/com/nemi/NemiApplication.java
@@ -2,8 +2,10 @@ package br.com.nemi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/api/.env")
 public class NemiApplication {
 
 	public static void main(String[] args) {
