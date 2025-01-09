fe7e9e75073a09525b1a9379b5a7129ce881c664
Mateus <mateusc.ramos@outlook.com>
2025-01-08 22:26:03 -0300

    feat: add Twilio client for sending messages

---
+++ api/src/main/java/br/com/nemi/util/TwilioClient.java
@@ -0,0 +1,28 @@
+package br.com.nemi.util;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TwilioClient {
+
+    @Value("${api.service.twilio.account-sid}")
+    private String TWILIO_ACCOUNT_SID;
+
+    @Value("${api.service.twilio.auth-token}")
+    private String TWILIO_AUTH_TOKEN;
+
+    public void sendMessage(String to) {
+        Twilio.init(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+        String FROM = "whatsapp:+14155238886";
+        Message.creator(
+                new PhoneNumber("whatsapp:" + to),
+                new PhoneNumber(FROM),
+                "Olá"
+        ).create();
+    }
+}
